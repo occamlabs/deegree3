e67ab9100358eef78d06294c4c702d0686019c83
Markus Schneider <schneider@occamlabs.de>
2013-10-07 08:51:17 +0200

    New module: deegree-layers-gdal

---
+++ deegree-layers/deegree-layers-gdal/src/main/java/org/deegree/layer/persistence/gdal/GdalLayerData.java
@@ -0,0 +1,90 @@
+/*----------------------------------------------------------------------------
+ This file is part of deegree
+ Copyright (C) 2001-2013 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+ and
+ - Occam Labs UG (haftungsbeschränkt) -
+ and others
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ e-mail: info@deegree.org
+ website: http://www.deegree.org/
+----------------------------------------------------------------------------*/
+package org.deegree.layer.persistence.gdal;
+
+import static java.awt.image.BufferedImage.TYPE_INT_RGB;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.List;
+
+import org.deegree.feature.FeatureCollection;
+import org.deegree.geometry.Envelope;
+import org.deegree.layer.LayerData;
+import org.deegree.rendering.r2d.context.RenderContext;
+import org.slf4j.Logger;
+
+/**
+ * {@link LayerData} implementation for layers that are drawn from GDAL datasets.
+ * 
+ * @author <a href="mailto:schneider@occamlabs.de">Markus Schneider</a>
+ * 
+ * @since 3.4
+ */
+class GdalLayerData implements LayerData {
+
+    private static final Logger LOG = getLogger( GdalLayerData.class );
+
+    private final List<File> files;
+
+    private final Envelope bbox;
+
+    private final int width;
+
+    private final int height;
+
+    GdalLayerData( List<File> files, Envelope bbox, int width, int height ) {
+        this.files = files;
+        this.bbox = bbox;
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public void render( RenderContext context ) {
+        try {
+            BufferedImage img = extractRegionFromGdalFiles();
+            context.paintImage( img );
+        } catch ( Throwable e ) {
+            LOG.trace( "Stack trace:", e );
+            LOG.error( "Unable to render raster: {}", e.getLocalizedMessage() );
+        }
+    }
+
+    private BufferedImage extractRegionFromGdalFiles() {
+        BufferedImage img = new BufferedImage( width, height, TYPE_INT_RGB );
+        return img;
+    }
+
+    @Override
+    public FeatureCollection info() {
+        throw new UnsupportedOperationException();
+    }
+
+}
---
+++ deegree-layers/deegree-layers-gdal/src/main/java/org/deegree/layer/persistence/gdal/GdalLayerStoreBuilder.java
@@ -0,0 +1,122 @@
+/*----------------------------------------------------------------------------
+ This file is part of deegree
+ Copyright (C) 2001-2013 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+ and
+ - Occam Labs UG (haftungsbeschränkt) -
+ and others
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ e-mail: info@deegree.org
+ website: http://www.deegree.org/
+----------------------------------------------------------------------------*/
+package org.deegree.layer.persistence.gdal;
+
+import static org.deegree.commons.ows.metadata.DescriptionConverter.fromJaxb;
+import static org.deegree.geometry.metadata.SpatialMetadataConverter.fromJaxb;
+import static org.deegree.layer.config.ConfigUtils.parseDimensions;
+import static org.deegree.layer.config.ConfigUtils.parseStyles;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.deegree.commons.ows.metadata.Description;
+import org.deegree.commons.utils.DoublePair;
+import org.deegree.commons.utils.Pair;
+import org.deegree.cs.coordinatesystems.ICRS;
+import org.deegree.geometry.metadata.SpatialMetadata;
+import org.deegree.layer.Layer;
+import org.deegree.layer.config.ConfigUtils;
+import org.deegree.layer.metadata.LayerMetadata;
+import org.deegree.layer.persistence.LayerStore;
+import org.deegree.layer.persistence.MultipleLayerStore;
+import org.deegree.layer.persistence.base.jaxb.ScaleDenominatorsType;
+import org.deegree.layer.persistence.gdal.jaxb.GDALLayerType;
+import org.deegree.layer.persistence.gdal.jaxb.GDALLayers;
+import org.deegree.style.se.unevaluated.Style;
+import org.deegree.workspace.ResourceBuilder;
+import org.deegree.workspace.ResourceMetadata;
+import org.deegree.workspace.Workspace;
+
+/**
+ * This class is responsible for building GDAL layer stores.
+ * 
+ * @author <a href="mailto:schneider@occamlabs.de">Markus Schneider</a>
+ * 
+ * @since 3.4
+ */
+class GdalLayerStoreBuilder implements ResourceBuilder<LayerStore> {
+
+    private GDALLayers cfg;
+
+    private Workspace workspace;
+
+    private ResourceMetadata<LayerStore> metadata;
+
+    GdalLayerStoreBuilder( GDALLayers cfg, Workspace workspace, ResourceMetadata<LayerStore> metadata ) {
+        this.cfg = cfg;
+        this.workspace = workspace;
+        this.metadata = metadata;
+    }
+
+    @Override
+    public LayerStore build() {
+        Map<String, Layer> layerNameToLayer = new HashMap<String, Layer>();
+        for ( GDALLayerType gdalLayerCfg : cfg.getGDALLayer() ) {
+            LayerMetadata md = buildLayerMetadata( gdalLayerCfg );
+            Pair<Map<String, Style>, Map<String, Style>> p = parseStyles( workspace, gdalLayerCfg.getName(),
+                                                                          gdalLayerCfg.getStyleRef() );
+            md.setStyles( p.first );
+            md.setLegendStyles( p.second );
+            List<File> resolvedFiles = resolveFiles( gdalLayerCfg.getFile() );
+            Layer layer = new GdalLayer( md, resolvedFiles );
+            layerNameToLayer.put( gdalLayerCfg.getName(), layer );
+        }
+        return new MultipleLayerStore( layerNameToLayer, metadata );
+    }
+
+    private List<File> resolveFiles( List<String> file ) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    private LayerMetadata buildLayerMetadata( GDALLayerType lay ) {
+        SpatialMetadata smd = fromJaxb( lay.getEnvelope(), lay.getCRS() );
+        Description desc = fromJaxb( lay.getTitle(), lay.getAbstract(), lay.getKeywords() );
+        LayerMetadata md = new LayerMetadata( lay.getName(), desc, smd );
+        md.setDimensions( parseDimensions( md.getName(), lay.getDimension() ) );
+        md.setMapOptions( ConfigUtils.parseLayerOptions( lay.getLayerOptions() ) );
+        md.setMetadataId( lay.getMetadataSetId() );
+        if ( smd.getEnvelope() == null ) {
+            // TODO envelope from GDAL file
+        }
+        if ( smd.getCoordinateSystems() == null || smd.getCoordinateSystems().isEmpty() ) {
+            List<ICRS> crs = new ArrayList<ICRS>();
+            crs.add( smd.getEnvelope().getCoordinateSystem() );
+            smd.setCoordinateSystems( crs );
+        }
+        ScaleDenominatorsType denoms = lay.getScaleDenominators();
+        if ( denoms != null ) {
+            md.setScaleDenominators( new DoublePair( denoms.getMin(), denoms.getMax() ) );
+        }
+        return md;
+    }
+}
