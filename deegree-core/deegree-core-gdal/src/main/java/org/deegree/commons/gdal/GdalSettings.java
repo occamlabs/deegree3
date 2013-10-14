789fa5dbb9afc1f6d2265bdc309cec5ac7429866
Markus Schneider <schneider@occamlabs.de>
2013-10-14 11:54:06 +0200

    Added new common module for gdal

---
+++ deegree-core/deegree-core-gdal/src/main/java/org/deegree/commons/gdal/GdalSettings.java
@@ -0,0 +1,28 @@
+package org.deegree.commons.gdal;
+
+import org.deegree.workspace.Destroyable;
+import org.deegree.workspace.Initializable;
+import org.deegree.workspace.Workspace;
+
+/**
+ * TODO add class documentation here
+ * 
+ * @author <a href="mailto:name@company.com">Your Name</a>
+ *
+ * @since 3.4
+ */
+public class GdalSettings implements Initializable, Destroyable {
+
+    @Override
+    public void destroy( Workspace workspace ) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void init( Workspace workspace ) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
