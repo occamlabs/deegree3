2a828db448ef9c028696def8dd4e7cd710075245
Markus Schneider <schneider@occamlabs.de>
2014-03-25 20:06:05 +0100

    Added LRU-mechanism for freeing resource slots

--- deegree-core/deegree-core-gdal/src/main/java/org/deegree/commons/gdal/pool/LimitedKeyedResourcePool.java
+++ deegree-core/deegree-core-gdal/src/main/java/org/deegree/commons/gdal/pool/LimitedKeyedResourcePool.java
@@ -27,6 +27,8 @@
 ----------------------------------------------------------------------------*/
 package org.deegree.commons.gdal.pool;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 import java.io.Closeable;
 import java.io.IOException;
 import java.util.HashMap;
@@ -36,9 +38,11 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+import org.slf4j.Logger;
+
 /**
  * A "keyed" resource pool implementation that pools {@link KeyedResource} object instances and guarantees a maximum
- * limit of open resources.
+ * limit fpr open resources.
  * 
  * @author <a href="mailto:schneider@occamlabs.de">Markus Schneider</a>
  * 
@@ -46,17 +50,17 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class LimitedKeyedResourcePool<T extends KeyedResource> implements Closeable {
 
+    private static final Logger LOG = getLogger( LimitedKeyedResourcePool.class );
+
     private final KeyedResourceFactory<T> factory;
 
     private final int maxResources;
 
     private final Map<String, BlockingQueue<T>> keyToIdleQueue = new HashMap<String, BlockingQueue<T>>();
 
-    private final BlockingQueue<T> returnQueue = new LinkedBlockingQueue<T>();
+    private final LruKeyTracker keyTracker;
 
-    private final Lock checkForAvailableResources = new ReentrantLock();
-
-    private int activeResources;
+    private final Lock needResource = new ReentrantLock();
 
     /**
      * Creates a new {@link LimitedKeyedResourcePool} instance.
@@ -69,47 +73,29 @@ public class LimitedKeyedResourcePool<T extends KeyedResource> implements Closea
     public LimitedKeyedResourcePool( final KeyedResourceFactory<T> factory, final int maxResources ) {
         this.factory = factory;
         this.maxResources = maxResources;
+        keyTracker = new LruKeyTracker( maxResources );
     }
 
     public T borrow( final String key )
                             throws InterruptedException, IOException {
 
-        checkForAvailableResources.lock();
-        copyReturnQueueToIdleQueues();
+        LOG.debug( "Borrowing resource, key: " + key + ". Total resource count: " + keyTracker );
+        needResource.lock();
         T resource = checkForIdleResource( key );
-        checkForAvailableResources.unlock();
-
-        if ( resource == null ) {
-            checkForAvailableResources.lock();
-            resource = ensureSlotForResource( key );
-            checkForAvailableResources.unlock();
-            if ( resource == null ) {
-                resource = factory.create( key );
-            }
-            if ( resource != null && !resource.getKey().equals( key ) ) {
-                resource.close();
-                resource = factory.create( key );
+        if ( resource != null ) {
+            recycleResource( resource );
+        } else if ( keyTracker.isEmptySlotsAvailable() ) {
+            resource = addResource( key );
+        } else {
+            resource = takeLeastRecentlyUsedResource();
+            if ( resource.getKey().equals( key ) ) {
+                recycleResource( resource );
+            } else {
+                resource = trashResourceAndAddNew( key, resource );
             }
         }
+        LOG.debug( "Borrowed resource, key: " + key + ". Total resource count: " + keyTracker );
         return resource;
-    }
-
-    public void returnObject( final T resource ) {
-        returnQueue.add( resource );
-    }
-
-    @Override
-    public void close() {
-
-    }
-
-    private void copyReturnQueueToIdleQueues() {
-        T resource = null;
-        while ( ( resource = returnQueue.poll() ) != null ) {
-            final String key = resource.getKey();
-            final BlockingQueue<T> queue = getQueue( key );
-            queue.add( resource );
-        }
     }
 
     private T checkForIdleResource( final String key ) {
@@ -117,26 +103,48 @@ public class LimitedKeyedResourcePool<T extends KeyedResource> implements Closea
         return queue.poll();
     }
 
-    private T ensureSlotForResource( String key )
-                            throws InterruptedException {
-        activeResources++;
-        T resource = null;
-        while ( activeResources > maxResources ) {
-            resource = takeNextCloseableResource();
-            activeResources--;
-        }
-        return resource;
+    private void recycleResource( final T resource ) {
+        LOG.debug( "Got recycled resource, key: " + resource.getKey() );
+        keyTracker.renew (resource.getKey());
+        needResource.unlock();
     }
 
-    private T takeNextCloseableResource()
+    private T addResource( final String key ) {
+        LOG.debug( "Got empty resource slot" );
+        keyTracker.add( key );
+        needResource.unlock();
+        LOG.debug( "Creating resource, key: " + key );
+        return factory.create( key );
+    }
+
+    private T trashResourceAndAddNew( final String key, final T resource )
+                            throws IOException {
+        LOG.debug( "Got resource to trash, key: " + resource.getKey() );
+        keyTracker.remove( resource.getKey() );
+        keyTracker.add( key );
+        needResource.unlock();
+        LOG.debug( "Destroying resource, key: " + resource.getKey() );
+        resource.close();
+        LOG.debug( "Creating resource, key: " + key );
+        return factory.create( key );
+    }
+
+    private T takeLeastRecentlyUsedResource()
                             throws InterruptedException {
-        for ( final BlockingQueue<T> queue : keyToIdleQueue.values() ) {
-            final T resource = queue.poll();
-            if ( resource != null ) {
-                return resource;
-            }
-        }
-        return returnQueue.take();
+        BlockingQueue<T> queue = getQueue( keyTracker.getLeastRecentlyUsedInstance() );
+        return queue.take();
+    }
+
+    public void returnObject( final T resource ) {
+        LOG.debug( "Returning resource, key: " + resource.getKey() + ". Total resource count: " + keyTracker );
+        final String key = resource.getKey();
+        final BlockingQueue<T> queue = getQueue( key );
+        queue.add( resource );
+    }
+
+    @Override
+    public void close() {
+
     }
 
     private synchronized BlockingQueue<T> getQueue( final String key ) {
